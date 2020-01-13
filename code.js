3407dd14a2598149d5058958b9ee347fbef5d2f3
Oscar Nunez <noscar07@gmail.com>
2020-01-12 20:00:01 -0500

    added description for project and started letter special and number list

--- code.js
+++ code.js
@@ -1,5 +1,23 @@
 /*
 
-Will make a random password generator for the website.
+Will make a random password generator for the website. The customer will chose how specific they want their password. The customer must chose at least one.
 
+customer password must have between 8-128 characters. 
+It must contain a 
+captital letter,
+lower case letter,
+special character,
+and a number.
+
+once the customer chose their criteria the generated password must appear on the page (or alert)
+
+Things to work on
+
+1, list the possible selections The computer can select.(letters, number and special character)
 */
+
+var characterList=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var numberList=[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+var specialChoice=["!", "#", "$", "%", "&", " ' ", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^","_", "`", "{", "|", "}", "~"]
